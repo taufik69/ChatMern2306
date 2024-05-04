6cb973e6b6011ed50ce629c42f5878c5f6403b78
taufik69 <taufik35-2915@diu.edu.bd>
2024-05-04 14:02:23 +0600

    homeleft design bug fixing

--- src/Component/HomeConponent/HomeLeft/HomeLeft.jsx
+++ src/Component/HomeConponent/HomeLeft/HomeLeft.jsx
@@ -9,23 +9,23 @@ const HomeLeft = ({ active }) => {
   console.log(active);
   return (
     <div>
-      <div className=" bg-btn-color px-10   h-[90vh]  py-[38px]">
+      <div className=" h-[90vh] bg-btn-color   px-10  py-[38px]">
         <picture>
           <img src={profilePicture} alt={profilePicture} />
         </picture>
         <div className="mt-16 ">
-          <ul className="flex flex-col justify-center items-center gap-y-10">
+          <ul className="flex flex-col items-center justify-center gap-y-10">
             <li
               className={
                 active === "home"
-                  ? " custom-border cursor-pointer bg-white w-[160%] ml-5 flex items-center justify-center py-4  rounded-l-lg  "
+                  ? "custom-border ml-5 flex w-[160%] cursor-pointer items-center justify-center rounded-l-lg bg-white  py-4  "
                   : "cursor-pointer"
               }
             >
               <img
                 src={home}
                 alt={home}
-                className="w-[50px] h-[50px] text-[#BAD1FF] flex justify-center items-center"
+                className="flex h-[50px] w-[50px] items-center justify-center text-[#BAD1FF]"
               />
             </li>
 
@@ -33,7 +33,7 @@ const HomeLeft = ({ active }) => {
               <img
                 src={el}
                 alt={el}
-                className="w-[50px] h-[50px] text-[#BAD1FF]"
+                className="h-[50px] w-[50px] text-[#BAD1FF]"
               />
             </li>
 
@@ -41,16 +41,16 @@ const HomeLeft = ({ active }) => {
               <img
                 src={bell}
                 alt={bell}
-                className="w-[50px] h-[50px] text-[#BAD1FF]"
+                className="h-[50px] w-[50px] text-[#BAD1FF]"
               />
             </li>
 
             <li className="cursor-pointer">
-              <IoSettingsOutline className="animate-spin w-[50px] h-[50px] text-[#BAD1FF]" />
+              <IoSettingsOutline className="h-[50px] w-[50px] animate-spin text-[#BAD1FF]" />
             </li>
 
             <li className="mt-16 cursor-pointer">
-              <CiLogout className="animate-pulse w-[50px] h-[50px] text-[#BAD1FF] font-black" />
+              <CiLogout className="h-[50px] w-[50px] animate-pulse font-black text-[#BAD1FF]" />
             </li>
           </ul>
         </div>

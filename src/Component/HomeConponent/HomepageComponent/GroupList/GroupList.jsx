7e3349468db592bee299aee2f9fe39c24cc3fdf9
taufik69 <taufik35-2915@diu.edu.bd>
2024-05-30 17:32:22 +0600

    group work start

--- src/Component/HomeConponent/HomepageComponent/GroupList/GroupList.jsx
+++ src/Component/HomeConponent/HomepageComponent/GroupList/GroupList.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoEllipsisVerticalSharp } from "react-icons/io5";
 
 import profilePicture from "../../../../assets/HomepageImage/one.gif";
 import profilePictur2 from "../../../../assets/HomepageImage/two.gif";
 import profilePicture3 from "../../../../assets/HomepageImage/one.gif";
 import profilePicture4 from "../../../../assets/HomepageImage/two.gif";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "60%",
+  },
+};
 const GroupList = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   const users = [
     {
       id: 1,
@@ -39,15 +60,25 @@ const GroupList = () => {
       button: "join",
     },
   ];
+  /**
+   * todo : HandleGroup funtionlaity
+   *
+   */
+
   return (
     <div className="w-[34%]">
       <div className="my-5 flex items-center justify-between">
         <h1 className="font-Poppins text-xl font-semibold text-custom-black">
           Groups List
         </h1>
-        <span>
-          <IoEllipsisVerticalSharp className="text-2xl text-btn-color" />
-        </span>
+
+        <button
+          onClick={() => setIsOpen(true)}
+          type="button"
+          className="relative inline-flex items-center rounded-lg bg-gradient-to-r from-[#614385] to-[#4a5dab]  px-5 py-2.5 text-center text-sm font-medium text-white "
+        >
+          Create Group
+        </button>
       </div>
       <div className="mt-10 h-[347px]  w-full overflow-y-scroll  rounded-xl shadow-xl scrollbar-thin  scrollbar-track-gray-400 scrollbar-thumb-sky-700">
         <div className=" divide-y-[1px] divide-gray-200">
@@ -81,6 +112,68 @@ const GroupList = () => {
           ))}
         </div>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <form class="mx-auto max-w-sm">
+          <div class="mb-5">
+            <label
+              for="email"
+              class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="name@flowbite.com"
+              required
+            />
+          </div>
+          <div class="mb-5">
+            <label
+              for="password"
+              class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              id="password"
+              class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              required
+            />
+          </div>
+          <div class="mb-5 flex items-start">
+            <div class="flex h-5 items-center">
+              <input
+                id="remember"
+                type="checkbox"
+                value=""
+                class="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
+                required
+              />
+            </div>
+            <label
+              for="remember"
+              class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Remember me
+            </label>
+          </div>
+          <button
+            type="submit"
+            class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </Modal>
     </div>
   );
 };
